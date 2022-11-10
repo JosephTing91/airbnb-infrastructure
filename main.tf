5e03907226522e5031ac68b8a8a88ec4159742c3
JosephTing91 <joeksting@gmail.com>
2022-11-09 17:56:54 -0700

    poo

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "web" {
     ami=    var.AMI_ID
     instance_type= var.instance_type
     monitoring= true
-    ebs_optimized=true
     root_block_device {
     encrypted     = true
     }
